ae7742bc64f12ea6646a8d0460b95508bbbc82a3
wtjerry <wtjerry123@gmail.com>
2018-09-29 19:02:45 +0200

    added comment a to excercise, 7, how to solve the 'out of local stack' issue in a different way

--- exercise2/exercise7.pl
+++ exercise2/exercise7.pl
@@ -4,7 +4,7 @@ mult(1,1,1).
 mult(1,X,X).
 mult(X,1,X).
 mult(X,Y,R) :-
-    X > 1,
+    X > 1,  % instead of those 2 checks a cut ('!') could be used as last goal to prevent backtracking
     Y > 1,
     Ysub1 is Y-1,
     mult(X,Ysub1,R1),
