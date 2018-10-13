55f9874a6b8f49061fb222b5ddc526b1a9fd900c
wtjerry <wtjerry123@gmail.com>
2018-10-13 14:32:43 +0200

    splitted relationship declation out of exercise4 file

--- exercise4/exercise4.pl
+++ exercise4/exercise4.pl
@@ -1,6 +1,7 @@
 :- use_module(library(http/json_convert)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/http_client)).
+:- consult(relationship).
 
 
 solve(relationship, Id) :-
@@ -44,43 +45,4 @@ post_relationship(Boolean, Id) :-
 :- json_object
     relationship(problemKey:integer, relationship:atom, firstPerson:atom, secondPerson:atom),
     relationship_solution(solution:atom, problemKey:integer).
-
-
-% ---- relationship problem declation
-
-female(mary). female(liz). female(mia). female(tina). female(ann). female(sue).
-male(mike). male(jack). male(fred). male(tom). male(joe). male(jim).
-parent(mary, mia). parent(mary, fred). parent(mary, tina).
-parent(mike, mia). parent(mike, fred). parent(mike, tina).
-parent(liz, tom). parent(liz, joe).
-parent(jack, tom). parent(jack, joe).
-parent(mia, ann).
-parent(tina, sue). parent(tina, jim).
-parent(tom, sue). parent(tom, jim).
-
-mother(X, Y) :-
-    female(X),
-    parent(X, Y).
-
-father(X, Y) :-
-    male(X),
-    parent(X, Y).
-
-sibling(X, Y) :-
-    parent(Z, X),
-    parent(Z, Y).
-
-grandmother(X, Y) :-
-    mother(X, Z),
-    parent(Z, Y).
-
-grandfather(X, Y) :-
-    father(X, Z),
-    parent(Z, Y).
-
-offspring(X, Y) :- 
-    parent(Y, X).
-offspring(X, Y) :- 
-    parent(Y, Z),
-    offspring(X, Z).
 
---
+++ exercise4/relationship.pl
@@ -0,0 +1,36 @@
+female(mary). female(liz). female(mia). female(tina). female(ann). female(sue).
+male(mike). male(jack). male(fred). male(tom). male(joe). male(jim).
+parent(mary, mia). parent(mary, fred). parent(mary, tina).
+parent(mike, mia). parent(mike, fred). parent(mike, tina).
+parent(liz, tom). parent(liz, joe).
+parent(jack, tom). parent(jack, joe).
+parent(mia, ann).
+parent(tina, sue). parent(tina, jim).
+parent(tom, sue). parent(tom, jim).
+
+mother(X, Y) :-
+    female(X),
+    parent(X, Y).
+
+father(X, Y) :-
+    male(X),
+    parent(X, Y).
+
+sibling(X, Y) :-
+    parent(Z, X),
+    parent(Z, Y).
+
+grandmother(X, Y) :-
+    mother(X, Z),
+    parent(Z, Y).
+
+grandfather(X, Y) :-
+    father(X, Z),
+    parent(Z, Y).
+
+offspring(X, Y) :- 
+    parent(Y, X).
+offspring(X, Y) :- 
+    parent(Y, Z),
+    offspring(X, Z).
+
