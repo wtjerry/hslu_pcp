46cb3da4828f603dfef5dfa42056d3f1505015e6
wtjerry <wtjerry123@gmail.com>
2018-10-18 11:16:00 +0200

    removed unnecessary comments

--- exercise4/exercise2.pl
+++ exercise4/exercise2.pl
@@ -2,22 +2,6 @@
 % true . 
 % ?- set_difference([1, 2, 3, 4, 5, 6], [2, 4, 6], L).
 % L = [1, 3, 5] .
-% set_difference([], _, []).
-% set_difference([In1Head | In1Tail], In2, Out) :-
-%     set_difference(In1Tail, In2, Out2),
-%     not(member(In1Head, In2)),
-%     Out = [In1Head | Out2].
-
-    
-% set_difference([], _, []).
-% set_difference([In1Head | In1Tail], In2, Out) :-
-%     not(member(In1Head, In2)),
-%     set_difference(In1Head, In2, Out2),
-%     Out = [In1Head | Out2].
-% 
-% set_difference([In1Head | In1Tail], In2, Out) :-
-%     member(In1Head, In2),
-%     set_difference(In1Head, In2, Out).
 
 set_difference([E | Set1], Set2, [E | SetDifference]) :-
     not(member(E, Set2)),
